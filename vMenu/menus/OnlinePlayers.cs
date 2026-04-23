0124d6873539e2198cf38a1babb523c0046e4589
ivanzhestkov99 <ivanzhestkov99@gmail.com>
2026-04-23 19:33:30 +0300

    Фикс индекса меню при обновлении списка

--- vMenu/menus/OnlinePlayers.cs
+++ vMenu/menus/OnlinePlayers.cs
@@ -367,6 +367,7 @@ namespace vMenuClient.menus
         {
             void UpdateStuff()
             {
+                var savedIndex = menu.CurrentIndex;
                 menu.ClearMenuItems();
 
                 foreach (var p in MainMenu.PlayersList.OrderBy(a => ResolveDisplayName(a)))
@@ -380,8 +381,15 @@ namespace vMenuClient.menus
                 }
 
                 menu.RefreshIndex();
+                if (menu.Size > 0)
+                {
+                    menu.CurrentIndex = Math.Min(savedIndex, menu.Size - 1);
+                }
                 //menu.UpdateScaleform();
-                playerMenu.RefreshIndex();
+                if (!playerMenu.Visible)
+                {
+                    playerMenu.RefreshIndex();
+                }
                 //playerMenu.UpdateScaleform();
             }
 
